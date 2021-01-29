3dd81033efdade9b9c00b2918c2201476e66525f
Victoria Konkiel <victoria.konkiel@yahoo.com>
2021-01-29 15:37:05 -0500

    react guide files up to lifecycle hooks

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+
+    // componentWillReceiveProps() {
+    //     console.log('[Persons.js] componentWillReceiveProps', props);
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return {message: 'Snapshot!'};
+    }
+
+    // componentWillUpdate() {
+
+    // }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
+    componentWillUnmount() {
+        console.log('[Persons.js] componentWillUnmount');
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...')
+        return this.props.persons.map((person, index) => {
+            return ( 
+                <Person 
+                    name={person.name} 
+                    age={person.age} 
+                    click={() => this.props.clicked(index)} 
+                    key={person.id}
+                    changed={(event) => this.props.changed(event, person.id)} />
+            )
+        
+        })
+    }
+}
+
+export default Persons;
---
+++ src/containers/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor')
+  }
+
+  state = {
+    persons : [
+      { id:"xjf9", name:"Victoria", age:28},
+      { id:"oefuhq", name:"Shanelle", age:26},
+      { id:"foiedh", name:"Cole", age:11}
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    showCockpit: true
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState( {persons : persons} );
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+
+  render() {
+    console.log('[App.js] render');
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />
+        </div>
+      )
+    }
+
+    return (
+      <div className={classes.App}>
+      <button onClick={() => {this.setState({ showCockpit: false})}}>Remove Cockpit</button>
+        {this.state.showCockpit ? <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        /> : null}
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
